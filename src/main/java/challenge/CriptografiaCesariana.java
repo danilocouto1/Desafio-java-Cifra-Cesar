f622d49f7571aeebd3b405f2eb556b4b7af4196b
Danilo Couto <dansantosalfa@gmail.com>
2021-12-10 12:07:42 -0300

    Função de criptografia de Cesar

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -4,11 +4,20 @@ public class CriptografiaCesariana implements Criptografia {
 
     @Override
     public String criptografar(String texto) {
-        throw new UnsupportedOperationException("esse method nao esta implementado aainda");
-    }
+        //throw new copiados da pasta de test
 
-    @Override
-    public String descriptografar(String texto) {
-        throw new UnsupportedOperationException("esse method nao esta implementado aainda");
+        String textoMin = texto.toLowerCase();
+        String textoFinal = "";
+        for (byte i = 0; i < textoMin.length(); i++) {
+            if (textoMin.charAt(i) == ' ' || textoMin.charAt(i) >= '0' && textoMin.charAt(i) <= '9') {
+                textoFinal += textoMin.charAt(i);
+            } else {
+                textoFinal += (char) (textoMin.charAt(i) + 3 );
+            }
+        }
+        if (texto == null) throw new NullPointerException();
+        if (texto == "") throw new IllegalArgumentException();
+
+        return textoFinal;
     }
 }
