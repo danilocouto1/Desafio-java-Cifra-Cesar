22ff59a4c59e7a4dba73bbb71143bf9442371531
Danilo Couto <dansantosalfa@gmail.com>
2021-12-10 12:07:59 -0300

    Função de descriptografia de Cesar

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -20,4 +20,23 @@ public class CriptografiaCesariana implements Criptografia {
 
         return textoFinal;
     }
+
+    @Override
+    public String descriptografar(String texto) {
+        //throw new copiados da pasta de test
+
+        String textoMin = texto.toLowerCase();
+        String textoFinal = "";
+        for (byte i = 0; i < textoMin.length(); i++) {
+            if (textoMin.charAt(i) == ' ' || textoMin.charAt(i) >= '0' && textoMin.charAt(i) <= '9') {
+                textoFinal += textoMin.charAt(i);
+            } else {
+                textoFinal += (char)( textoMin.charAt(i) - 3);
+            }
+        }
+        if (texto == null) throw new NullPointerException();
+        if (texto == "") throw new IllegalArgumentException();
+
+        return textoFinal;
+    }
 }
